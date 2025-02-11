fe136647cebb42f507f4b9e79c8fec9e6d859b29
Davi Antonio Ramos Araujo <124791732+davizin-ra@users.noreply.github.com>
2025-02-11 12:48:49 -0300

    Add files via upload

---
+++ Aula1/Ex 2/index.php
@@ -0,0 +1,17 @@
+<?php
+    $nota1 = 10;
+    $nota2 = 3;
+    $nota3 = 10;
+
+    $notatotal = ($nota1 + $nota2 + $nota3)/3;
+
+    if ($notatotal >= 6){
+        $resultado = "Você foi aprovado";
+    }
+    else{
+        $resultado = "Você foi reprovado";
+    }
+
+    echo "Sua média foi:", $notatotal, "<br>";
+    echo round($resultado);
+?>
