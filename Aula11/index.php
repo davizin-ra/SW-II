e8b096208a6f76880828f1d0f563d9428f381401
Davi Antonio Ramos Araujo <124791732+davizin-ra@users.noreply.github.com>
2025-05-19 22:24:52 -0300

    Add files via upload

--- Aula11/index.php
+++ Aula11/index.php
@@ -1,9 +1,13 @@
 <?php
 
-    $busca = isset($_GET['entrada']) ? strtolower(trim($_GET['entrada'])) :'';
+$busca = isset($_GET['entrada']) ? preg_replace('/\D/', '', $_GET['entrada']) : '';
+$cepValido = preg_match('/^\d{8}$/', $busca);
+
+$cep = null;
+if ($cepValido) {
     $url = "https://viacep.com.br/ws/$busca/json/";
     $response = @file_get_contents($url);
-    $cep = json_decode($response, true);
+    $cep = json_decode($response, true);}
 
 ?>
 
@@ -14,23 +18,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cep</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
     <form method="get">
-        <input type="number" name="entrada" value="<?php echo htmlspecialchars($busca)?>">
+        <input type="number" name="entrada" maxlength="8" pattern="\d{8}" oninput="this.value = this.value.replace(/[^0-9]/g, '')"  value="<?php echo htmlspecialchars($busca) ?>" required placeholder="Digite o CEP (EX: 12345678)">
         <button>Enviar</button>
     </form>
 
     <div class="container">
-    <?php
+        <!-- <php
+    if (isset($_GET["entrada"]) && $_GET["entrada"]){
         echo $cep ['logradouro'];
         echo $cep ['bairro'];
         echo $cep ['localidade'];
         echo $cep ['estado'];
         echo $cep ['uf'];
-    ?>
+    }
+    ?> -->
+
+<?php
+if (isset($_GET["entrada"])) {
+    if (!$cepValido) {
+        echo "<p>CEP inválido! Digite exatamente 8 números.</p>";
+    } elseif (isset($cep['erro']) && $cep['erro'] == true) {
+        echo "<p>CEP não encontrado.</p>";
+    } else {
+        echo "<p><strong>Logradouro:</strong> {$cep['logradouro']}</p>";
+        echo "<p><strong>Bairro:</strong> {$cep['bairro']}</p>";
+        echo "<p><strong>Cidade:</strong> {$cep['localidade']}</p>";
+        echo "<p><strong>Estado (UF):</strong> {$cep['uf']}</p>";
+    }
+}
+?>
+
+
     </div>
 </body>
 
